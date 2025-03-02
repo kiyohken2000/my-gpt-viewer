1c15baac5cd12d84451d21d9c907c5bb1dde7946
kiyohken2000 <www.broadwell.com@gmail.com>
2025-03-02 18:24:53 +0900

    master

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -7,6 +7,7 @@ export default function PageHeader() {
     <div className="application">
       <Helmet>
         <meta charSet="utf-8" />
+        <meta name="google-adsense-account" content="ca-pub-9747065248920607"></meta>
         <title>{config.siteTitle}</title>
         <link rel="canonical" href={config.siteUrl} />
       </Helmet>
