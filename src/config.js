be273aedae51511b80cd8549d28fa7764650ea8e
kiyohken2000 <www.broadwell.com@gmail.com>
2026-02-18 20:58:10 +0900

    master

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const stripeLink = 'https://buy.stripe.com/6oE7vQ7VX0F14b6144'
 const stripeLinkAdd = 'https://buy.stripe.com/9B67sN9bt6Nv5yQbZbaVa01'
 
 const basename = ''
-const version = '1.0.33'
+const version = '1.0.34'
 const isDevMode = false
 
 const storeLinks = {
